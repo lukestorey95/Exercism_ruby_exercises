6ac9d9536e2d2632fc2369d9c3a692bc748afd49
Luke Storey <lukemstorey@gmail.com>
2022-04-06 20:32:33 +0100

    Complete scrabble_score

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,7 +1,27 @@
-=begin
-Write your code for the 'Scrabble Score' exercise in this file. Make the tests in
-`scrabble_score_test.rb` pass.
+class Scrabble
 
-To get started with TDD, see the `README.md` file in your
-`ruby/scrabble-score` directory.
-=end
+  LETTER_VALUES = {
+    /[AEIOULNRST]/ => 1,
+    /[DG]/ => 2,
+    /[BCMP]/ => 3,
+    /[FHVWY]/ => 4,
+    /[K]/ => 5,
+    /[JX]/ => 8,
+    /[QZ]/ => 10
+  }
+
+  def initialize(word)
+    @word = word.to_s.upcase.strip
+  end
+
+  def self.score(word)
+    new(word).score
+  end
+
+  def score
+    word_score = 0
+
+    LETTER_VALUES.inject(0) do |word_score, (letters, value)| word_score += (@word.scan(letters).count * value)
+    end
+  end
+end
