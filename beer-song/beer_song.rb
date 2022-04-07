4cdfc36cf1a96924ba0a13efb505204de6ef1f46
Luke Storey <lukemstorey@gmail.com>
2022-04-07 16:53:31 +0100

    Complete beersong exercism

--- beer-song/beer_song.rb
+++ beer-song/beer_song.rb
@@ -1,7 +1,21 @@
-=begin
-Write your code for the 'Beer Song' exercise in this file. Make the tests in
-`beer_song_test.rb` pass.
+class BeerSong
 
-To get started with TDD, see the `README.md` file in your
-`ruby/beer-song` directory.
-=end
+  def self.verses(beers)
+    if beers > 2
+      "#{beers} bottles of beer on the wall, #{beers} bottles of beer.\nTake one down and pass it around, #{beers-1} bottles of beer on the wall.\n"
+    elsif beers == 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    elsif beers == 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    else
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    end
+  end
+
+  def self.recite(start, finish)
+    (start..(start-finish+1)).step(-1).map do |beers|
+      verses(beers)
+    end
+    .join("\n")
+  end
+end
