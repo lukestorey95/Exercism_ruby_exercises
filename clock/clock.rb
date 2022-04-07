3d0025523f60da65905cc7614e933549d7b97a0a
Luke Storey <lukemstorey@gmail.com>
2022-04-07 14:52:22 +0100

    Complete exercism clock

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,7 +1,28 @@
-=begin
-Write your code for the 'Clock' exercise in this file. Make the tests in
-`clock_test.rb` pass.
+class Clock
+  attr_reader :hour, :minute
 
-To get started with TDD, see the `README.md` file in your
-`ruby/clock` directory.
-=end
+  def initialize(hour: 0,minute: 0)
+    @hour = (hour + minute / 60) % 24 
+    @minute = minute % 60
+  end
+
+  def +(other)
+    hour = self.hour + other.hour
+    minute = self.minute + other.minute
+    Clock.new(hour: hour, minute: minute)
+  end
+
+  def -(other)
+    hour = self.hour - other.hour
+    minute = self.minute - other.minute
+    Clock.new(hour: hour, minute: minute)
+  end
+
+  def ==(other)
+    hour == other.hour && minute == other.minute
+  end
+
+  def to_s
+    format("%02d:%02d", hour, minute)
+  end
+end
