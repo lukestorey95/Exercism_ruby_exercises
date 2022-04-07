5817e3b83f56117af22e5eafb41ec231ebc0c0f2
Luke Storey <lukemstorey@gmail.com>
2022-04-07 15:38:37 +0100

    Complete mircrowave exercism

--- microwave/microwave.rb
+++ microwave/microwave.rb
@@ -1,7 +1,12 @@
-=begin
-Write your code for the 'Microwave' exercise in this file. Make the tests in
-`microwave_test.rb` pass.
+class Microwave
+  def initialize(time)
+    @time = time
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/microwave` directory.
-=end
+  def timer
+    minutes, seconds = @time.divmod(100)
+    minutes += seconds / 60
+    seconds %= 60
+    format("%02d:%02d", minutes, seconds)
+  end
+end
