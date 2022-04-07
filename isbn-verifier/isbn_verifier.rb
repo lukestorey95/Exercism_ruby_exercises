b66aaee3209900c1e41c06196a80f8640d5e4170
Luke Storey <lukemstorey@gmail.com>
2022-04-07 17:37:36 +0100

    Complete isbn exercism

--- isbn-verifier/isbn_verifier.rb
+++ isbn-verifier/isbn_verifier.rb
@@ -1,7 +1,13 @@
-=begin
-Write your code for the 'ISBN Verifier' exercise in this file. Make the tests in
-`isbn_verifier_test.rb` pass.
+class IsbnVerifier
+  def self.valid?(string)
+    digits = string.scan(/\d|X/)
 
-To get started with TDD, see the `README.md` file in your
-`ruby/isbn-verifier` directory.
-=end
+    return false unless digits.count == 10
+
+    digits[-1] = 10 if digits[-1] == 'X'
+
+    digits.map.with_index do |n, i| 
+      n.to_i * (i+1)
+    end.sum % 11 == 0
+  end 
+end
