0f3650f5572290164ac5b0d111b0ae33ce8add3e
Luke Storey <lukemstorey@gmail.com>
2022-04-12 16:25:54 +0100

    Complete triangle exercise

--- triangle/triangle.rb
+++ triangle/triangle.rb
@@ -1,7 +1,22 @@
-=begin
-Write your code for the 'Triangle' exercise in this file. Make the tests in
-`triangle_test.rb` pass.
+class Triangle
+  def initialize(sides)
+    @sides = sides
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/triangle` directory.
-=end
+  def valid_triangle?
+    # return false if @sides.any? { |side| side.zero? }
+    @sides.max < @sides.sum/2.0
+  end
+
+  def equilateral?
+    self.valid_triangle? && @sides[0] == @sides[1] && @sides[1] == @sides[2]
+  end
+  
+  def scalene?
+    self.valid_triangle? && @sides[0] != @sides[1] && @sides[1] != @sides[2]
+  end
+
+  def isosceles?
+    self.valid_triangle? && @sides.sort.each_cons(2).any? { |pair| pair[0] == pair[1] }
+  end
+end
