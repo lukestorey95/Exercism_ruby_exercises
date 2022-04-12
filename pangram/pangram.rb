08e562832943e640f8798940e93977c35f60882e
Luke Storey <lukemstorey@gmail.com>
2022-04-12 11:53:28 +0100

    Complete pangram exercise

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,7 +1,6 @@
-=begin
-Write your code for the 'Pangram' exercise in this file. Make the tests in
-`pangram_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/pangram` directory.
-=end
+class Pangram
+  ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+  def self.pangram?(sentence)
+    sentence.downcase.scan(/[a-z]/).uniq.length == ALPHABET.length
+  end
+end
