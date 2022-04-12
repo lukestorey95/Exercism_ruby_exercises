17808547b926de1390c33e0a7b38327ed597f375
Luke Storey <lukemstorey@gmail.com>
2022-04-12 11:19:40 +0100

    Complete gigasecond exercise

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -1,7 +1,6 @@
-=begin
-Write your code for the 'Gigasecond' exercise in this file. Make the tests in
-`gigasecond_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/gigasecond` directory.
-=end
+class Gigasecond
+  GIGASECOND = 1000000000
+  def self.from(time)
+    time + GIGASECOND
+  end
+end
