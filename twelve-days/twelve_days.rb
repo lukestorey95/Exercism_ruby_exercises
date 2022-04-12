1e31731aee2b97ad2ab12e2ba67a9bab3b3b4e0e
Luke Storey <lukemstorey@gmail.com>
2022-04-12 09:27:05 +0100

    Complete twelve-days exercise

--- twelve-days/twelve_days.rb
+++ twelve-days/twelve_days.rb
@@ -1,7 +1,43 @@
-=begin
-Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-`twelve_days_test.rb` pass.
+class TwelveDays
+  GIFTS =[
+    "a Partridge in a Pear Tree.",
+    "two Turtle Doves, and ",
+    "three French Hens, ",
+    "four Calling Birds, ",
+    "five Gold Rings, ",
+    "six Geese-a-Laying, ",
+    "seven Swans-a-Swimming, ",
+    "eight Maids-a-Milking, ",
+    "nine Ladies Dancing, ",
+    "ten Lords-a-Leaping, ",
+    "eleven Pipers Piping, ",
+    "twelve Drummers Drumming, "
+  ]
 
-To get started with TDD, see the `README.md` file in your
-`ruby/twelve-days` directory.
-=end
+  DAYS = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth"
+  ]
+
+  def self.song
+    day = 0
+    song = []
+    until day == 12 do 
+      song << "On the #{DAYS[day]} day of Christmas my true love gave to me: #{GIFTS[0..day].reverse.join}\n"
+      day += 1
+    end
+    song.join("\n")
+  end
+end
+
+TwelveDays.song
