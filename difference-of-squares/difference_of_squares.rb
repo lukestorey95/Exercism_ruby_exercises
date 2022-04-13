a8ae0e066e8fff5c6198bceded7422c24e2b0207
Luke Storey <lukemstorey@gmail.com>
2022-04-13 09:13:47 +0100

    Complete squares exercise

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,7 +1,25 @@
 =begin
-Write your code for the 'Difference Of Squares' exercise in this file. Make the tests in
-`difference_of_squares_test.rb` pass.
+The square of the sum of the first ten natural numbers is
+(1 + 2 + ... + 10)² = 55² = 3025.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/difference-of-squares` directory.
+The sum of the squares of the first ten natural numbers is
+1² + 2² + ... + 10² = 385.
 =end
+
+class Squares
+  def initialize(num)
+    @num = num
+  end
+
+  def square_of_sum
+    (1..@num).sum ** 2
+  end
+
+  def sum_of_squares
+    (1..@num).map { |n| n ** 2 }.sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
